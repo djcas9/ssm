826da3304747248a6129dcb20e227101c95fcde4
Dustin Willis Webber <dustin.webber@gmail.com>
2015-10-07 15:26:46 -0400

    example update

--- examples/basic.go
+++ examples/basic.go
@@ -23,39 +23,40 @@ func main() {
 		Email: "cool@cool.com",
 	}
 
-	person.State = ssm.NewStateMachine(ssm.States{{
-		Name:    "normal",
-		Initial: true,
-		BeforeEnter: func() {
+	person.State = ssm.NewStateMachine(ssm.States{
+		{
+			Name:    "normal",
+			Initial: true,
+			BeforeEnter: func() {
+			},
+			BeforeExit: func() {
+			},
+		}, {
+			Name: "mad",
+			From: ssm.StateList{
+				"happy",
+				"sad",
+				"normal",
+			},
+			To: ssm.StateList{"happy"},
+			AfterEnter: func() {
+				person.Drunk = true
+			},
+		}, {
+			Name: "happy",
+			From: ssm.StateList{"mad", "sad", "normal"},
+			To:   ssm.StateList{"mad", "sad", "normal"},
+			BeforeEnter: func() {
+				fmt.Println("THINGS ARE LOOKING UP.")
+			},
+			AfterExit: func() {
+				fmt.Println("Feeling less awesome.")
+			},
+		}, {
+			Name: "sad",
+			From: ssm.StateList{"mad", "happy"},
+			To:   ssm.StateList{"happy", "normal", "mad"},
 		},
-		BeforeExit: func() {
-		},
-	}, {
-		Name: "mad",
-		From: ssm.StateList{
-			"happy",
-			"sad",
-			"normal",
-		},
-		To: ssm.StateList{"happy"},
-		AfterEnter: func() {
-			person.Drunk = true
-		},
-	}, {
-		Name: "happy",
-		From: ssm.StateList{"mad", "sad", "normal"},
-		To:   ssm.StateList{"mad", "sad", "normal"},
-		BeforeEnter: func() {
-			fmt.Println("THINGS ARE LOOKING UP.")
-		},
-		AfterExit: func() {
-			fmt.Println("Feeling less awesome.")
-		},
-	}, {
-		Name: "sad",
-		From: ssm.StateList{"mad", "happy"},
-		To:   ssm.StateList{"happy", "normal", "mad"},
-	},
 	})
 
 	if err := person.State.NewEvent(ssm.Event{
